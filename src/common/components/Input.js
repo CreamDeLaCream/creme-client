aef4829c5c2892790efda2f18d3699894756bfa5
greencoffee1635 <166354@naver.com>
2022-02-21 10:23:24 +0900

    [#5] edit: Input.js 불필요한 주석 삭제

--- src/common/components/Input.js
+++ src/common/components/Input.js
@@ -46,30 +46,6 @@ const Input = (props) => {
     </Grid>
   );
 };
-//   return (
-//     <>
-//       <InputForm />
-//     </>
-//   );
-// };
-
-// const InputForm = styled.input`
-//   width: 100%;
-//   height: 2.4rem;
-//   border: 2px solid var(--lightgray);
-//   border-radius: 5px;
-//   padding: 0px 0px 0px 5px;
-//   /* margin-bottom: 2rem; */
-//   font-size: 1.3rem;
-//   /* margin: 0 0 0 1.5rem; */
-//   color: gray;
-//   :focus {
-//     outline: none;
-//   }
-//   ${({ theme }) => theme.device.mobile} {
-//     min-width: 20rem;
-//   }
-// `;
 
 const TextareaForm = styled.textarea`
   border: 1px solid #212121;
@@ -95,11 +71,5 @@ const InputForm = styled.input`
     min-width: 20rem;
   }
 `;
-
-// const InputWrapper = styled.div`
-//   display: flex;
-//   align-items: center;
-//   margin-bottom: 2rem;
-// `;
 
 export default Input;
