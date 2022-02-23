5b69e199a1972fadaf7f62b0b02eb96c0ba388c2
greencoffee1635 <166354@naver.com>
2022-02-23 18:16:25 +0900

    [#17] test: redux 테스트 코드 추가

---
+++ src/common/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userSlice from './modules/userSlice';
+
+export default configureStore({
+  reducer: {
+    user: userSlice,
+    // post: postSlice,
+  },
+});
