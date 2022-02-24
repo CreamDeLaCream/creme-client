60db2feaa04e8acad9ac99bd5ed91597b217aaad
leaisrevolution <leaisrevolution@gmail.com>
2022-02-24 23:44:32 +0900

    [#28] feat : 인트로 페이지 세팅

---
+++ src/pages/Intro/Intro1st.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Intro1st = () => {
+  return <div>Intro1st</div>;
+};
---
+++ src/pages/Intro/Intro2nd.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Intro2nd = () => {
+  return <div>Intro2nd</div>;
+};
