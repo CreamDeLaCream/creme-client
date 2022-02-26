57eab5dbd7ddb144dfa0120705ce986128c2834e
greencoffee1635 <166354@naver.com>
2022-02-27 04:01:45 +0900

    [#19] edit: 불필요한 파일 삭제

--- src/common/redux/store.js
+++
@@ -1,9 +0,0 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import userSlice from './modules/userSlice';
-
-// export default configureStore({
-//   reducer: {
-//     user: userSlice,
-//     // post: postSlice,
-//   },
-// });
