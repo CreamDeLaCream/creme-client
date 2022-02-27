1726e579b98ace315075c5a731261f881ce95462
greencoffee1635 <166354@naver.com>
2022-02-27 15:18:51 +0900

    [#38] fix: GridLine 사라지지 않는 문제 해결

--- src/pages/Result/BarChart.js
+++ src/pages/Result/BarChart.js
@@ -28,9 +28,11 @@ const BarChart = () => {
         label: '지루함',
         data: [20],
         // 바탕색
-        backgroundColor: ['rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)'],
+        backgroundColor: ['rgba(255, 206, 86, 0.2)'],
+        // backgroundColor: ['rgba(214, 180, 161, 0.2)'],
         // 테두리색
-        borderColor: ['rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)'],
+        borderColor: ['rgba(255, 206, 86, 1)'],
+        // borderColor: ['rgba(214, 180, 161, 1)'],
         // 테두리 선 굵기
         borderWidth: 2,
       },
@@ -38,61 +40,105 @@ const BarChart = () => {
         label: '행복함',
         data: [80],
         backgroundColor: ['rgba(75, 192, 192, 0.2)'],
+        // backgroundColor: ['rgba(214, 180, 161, 0.2)'],
         borderColor: ['rgba(75, 192, 192, 1)'],
+        // borderColor: ['rgba(214, 180, 161, 1)'],
         borderWidth: 2,
       },
     ],
   };
 
+  // const options = {
+  //   indexAxis: 'y',
+  //   responsive: true,
+  //   plugins: {
+  //     legend: {
+  //       // position: 'right',
+  //       display: true,
+  //     },
+  //     title: {
+  //       display: false,
+  //       text: 'Chart.js Horizontal Bar Chart',
+  //     },
+  //   },
+  //   options: {
+  //     scales: {
+  //       xAxes: [
+  //         {
+  //           gridLines: {
+  //             display: false,
+  //             // drawBorder: false,
+  //             // borderDash: [3, 3],
+  //             // zeroLineColor: 'blue',
+  //           },
+
+  //           categoryPercentage: 0.7,
+  //           // barPercentage: 0.9,
+  //           ticks: {
+  //             beginAtZero: true,
+  //           },
+  //         },
+  //       ],
+  //       yAxes: [
+  //         {
+  //           display: false,
+  //           gridLines: {
+  //             display: false,
+  //             // zeroLineColor: 'transparent',
+  //           },
+  //           ticks: {
+  //             beginAtZero: true,
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   },
+  // };
+
   const options = {
     indexAxis: 'y',
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    // legend: {
+    //   display: true,
+    // },
+    scales: {
+      xAxis: {
+        max: 100,
+        grid: {
+          color: '#00000000',
+        },
+        ticks: {
+          display: false,
+        },
+      },
+      yAxis: {
+        grid: {
+          color: '#00000000',
+        },
+        ticks: {
+          display: false,
+        },
+      },
+    },
     responsive: true,
     plugins: {
       legend: {
-        // position: 'right',
-        display: false,
+        position: 'top',
+        display: true,
       },
       title: {
         display: false,
         text: 'Chart.js Horizontal Bar Chart',
       },
     },
-    options: {
-      scales: {
-        xAxes: [
-          {
-            gridLines: {
-              display: false,
-              // drawBorder: false,
-              // borderDash: [3, 3],
-              // zeroLineColor: 'blue',
-            },
-
-            categoryPercentage: 0.7,
-            barPercentage: 0.9,
-            ticks: {
-              beginAtZero: true,
-            },
-          },
-        ],
-        yAxes: [
-          {
-            display: false,
-            gridLines: {
-              display: false,
-              // zeroLineColor: 'transparent',
-            },
-            ticks: {
-              beginAtZero: true,
-            },
-          },
-        ],
-      },
-    },
   };
 
   return (
-    <div>
+    <div style={{}}>
       <Bar data={data} options={options} height={3} width={10} />
     </div>
   );
