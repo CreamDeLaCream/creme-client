19271276f81835d29ef6437720432965a5124938
greencoffee1635 <166354@naver.com>
2022-02-27 23:53:01 +0900

    [#41] feat: 유저 라이프 스타일 입력 페이지 ui 구성 및 입력 기능 추가

---
+++ src/pages/AddUserLife/AddUserLifePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// components
+import { Container, Header, Navbar } from '../../common/components';
+
+import InputBox from './InputBox';
+
+const AddUserLifePage = (props) => {
+  return (
+    <Container>
+      <Header />
+      <Navbar />
+      <InputBox />
+    </Container>
+  );
+};
+
+export default AddUserLifePage;
