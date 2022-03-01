e950d07f968fe033c677e3e3848e79b0633df537
greencoffee1635 <166354@naver.com>
2022-03-02 00:41:09 +0900

    [#37] edit: 마이펫 모달 삭제

--- src/pages/Result/ResultPage.js
+++ src/pages/Result/ResultPage.js
@@ -13,10 +13,8 @@ import {
   Navbar,
   Container,
   Input,
-  Modal,
 } from '../../common/components';
 import Dropdown from '../Result/Dropdown';
-import AddPetModal from '../MyPet/AddPetModal';
 import { resultData } from './ResultData';
 import { Keywords } from '../../common/components/Keyword';
 import CopyURL from './CopyURL';
@@ -31,18 +29,6 @@ const ResultPage = (props) => {
   useEffect(() => {
     console.log(id);
   }, [id]);
-  // const checkTab = (keyword) => {
-  //   if (history.location.pathname.includes(keyword) === true)
-  //    return true;
-  //   else return false;
-  // };
-
-  // (임시) 마이펫 페이지 모달
-  const [modalOpen, setModalOpen] = useState(false);
-
-  // _onClick={() => {
-  //   setModalOpen(true);
-  // }}
 
   // React.useEffect(() => {
   //   if (post_list.length === 0) {
@@ -56,184 +42,169 @@ const ResultPage = (props) => {
   // }, []);
 
   return (
-    <>
-      <Container>
-        <Header />
-        <Navbar />
+    <Container>
+      <Header />
+      <Navbar />
 
-        <Grid margin="1rem auto">
-          <Text type="mainTitle" color="var(--main)">
-            반려견 감정 상태 결과
-          </Text>
-        </Grid>
+      <Grid margin="1rem auto">
+        <Text type="mainTitle" color="var(--main)">
+          반려견 감정 상태 결과
+        </Text>
+      </Grid>
 
-        <ResultBox>
-          <img width="100%" src={resultData[0].dog_img} alt="dog_image" />
-        </ResultBox>
+      <ResultBox>
+        <img width="100%" src={resultData[0].dog_img} alt="dog_image" />
+      </ResultBox>
 
-        <Grid is_flex_end margin="-2rem 0 0 0">
-          <ButtonWrapper>
-            {/* <Grid is_flex width="13rem"> */}
-            <Button
-              circle
-              foldSize
-              size="3.5"
-              bg="var(--white)"
-              border="0.15rem solid var(--gray)"
-              is_flex_center
-              cursor
-            >
-              <BsHeartFill size="1.4rem" color="red" />
-            </Button>
-            <CopyURL />
-            <Button
-              circle
-              foldSize
-              size="3.5"
-              bg="var(--white)"
-              border="0.15rem solid var(--gray)"
-              is_flex_center
-              cursor
-            >
-              <p>Kakao</p>
-            </Button>
-          </ButtonWrapper>
-        </Grid>
+      <Grid is_flex_end margin="-2rem 0 0 0">
+        <ButtonWrapper>
+          <Button
+            circle
+            foldSize
+            size="3.5"
+            bg="var(--white)"
+            border="0.15rem solid var(--gray)"
+            is_flex_center
+            cursor
+          >
+            <BsHeartFill size="1.4rem" color="red" />
+          </Button>
+          <CopyURL />
+          <Button
+            circle
+            foldSize
+            size="3.5"
+            bg="var(--white)"
+            border="0.15rem solid var(--gray)"
+            is_flex_center
+            cursor
+          >
+            <p>Kakao</p>
+          </Button>
+        </ButtonWrapper>
+      </Grid>
 
-        <Grid margin="2rem auto">
-          <Text type="mainTitle" color="var(--main)" marginBottom="2rem">
-            {resultData[0].dog_name}, {resultData[0].dog_age}살
-          </Text>
-          <Text type="subTitle">
-            <Keywords typekeywords={resultData[0].dog.emotion} />
-            {/* TODO: Keyword에 props에 .character 추가하기 */}
-            {/* <Keyword /> */}
-          </Text>
-        </Grid>
-        <Grid margin="2rem auto">
-          <Text type="subTitle" color="var(--main)" marginBottom="2rem">
-            당신이 생각하는 {resultData[0].dog_name}의 감정상태 일치도는?
-          </Text>
+      <Grid margin="2rem auto">
+        <Text type="mainTitle" color="var(--main)" marginBottom="2rem">
+          {resultData[0].dog_name}, {resultData[0].dog_age}살
+        </Text>
+        <Text type="subTitle">
+          <Keywords typekeywords={resultData[0].dog.emotion} />
+          {/* TODO: Keyword에 props에 .character 추가하기 */}
+          {/* <Keyword /> */}
+        </Text>
+      </Grid>
+      <Grid margin="2rem auto">
+        <Text type="subTitle" color="var(--main)" marginBottom="2rem">
+          당신이 생각하는 {resultData[0].dog_name}의 감정상태 일치도는?
+        </Text>
 
-          <Grid is_flex mobileColumn>
-            <Grid display="flex" jutiyfyContent="space-around">
-              <Grid>
-                <Text
-                  center
-                  fontSize="5rem"
-                  color="var(--main)"
-                  whiteSpace="nowrap"
-                >
-                  {resultData[0].match === true ? (
-                    <div>일치</div>
-                  ) : (
-                    <div>불일치</div>
-                  )}
-                </Text>
-              </Grid>
-            </Grid>
-
+        <Grid is_flex mobileColumn>
+          <Grid display="flex" jutiyfyContent="space-around">
             <Grid>
-              <BarChart
-                data={[
-                  {
-                    label: resultData[0].dog.emotion,
-                    value: resultData[0].dog.prob * 100,
-                  },
-                  {
-                    label: resultData[0].human_emotion.emotion,
-                    value: resultData[0].human_emotion.prob * 100,
-                  },
-                ]}
-              />
+              <Text
+                center
+                fontSize="5rem"
+                color="var(--main)"
+                whiteSpace="nowrap"
+              >
+                {resultData[0].match === true ? (
+                  <div>일치</div>
+                ) : (
+                  <div>불일치</div>
+                )}
+              </Text>
             </Grid>
           </Grid>
-        </Grid>
-
-        <Grid margin="2rem auto">
-          <Text type="subTitle" color="var(--main)" marginBottom="15px">
-            현재 {resultData[0].dog_name}의 감정 상태
-          </Text>
 
           <Grid>
-            {/* <Text type="body" color="var(--darkcream)" marginBottom="10px">
-              #행복함 #즐거움 #여유로움 #슬픔
-            </Text> */}
-
-            <Text type="body" color="var(--deepcream)">
-              {resultData[0].emotion_description}
-            </Text>
+            <BarChart
+              data={[
+                {
+                  label: resultData[0].dog.emotion,
+                  value: resultData[0].dog.prob * 100,
+                },
+                {
+                  label: resultData[0].human_emotion.emotion,
+                  value: resultData[0].human_emotion.prob * 100,
+                },
+              ]}
+            />
           </Grid>
         </Grid>
-        <Grid margin="2rem auto">
-          <Text type="subTitle" color="var(--main)" marginBottom="15px">
-            {resultData[0].dog_name}의 솔루션
-          </Text>
+      </Grid>
+
+      <Grid margin="2rem auto">
+        <Text type="subTitle" color="var(--main)" marginBottom="15px">
+          현재 {resultData[0].dog_name}의 감정 상태
+        </Text>
+
+        <Grid>
           <Text type="body" color="var(--deepcream)">
-            {resultData[0].solution[1]}
-            &nbsp;
-            {resultData[0].solution[2]}
-            &nbsp;
-            {resultData[0].solution[3]}
-            &nbsp;
-            {resultData[0].solution[4]}
+            {resultData[0].emotion_description}
           </Text>
         </Grid>
-        <Grid margin="2rem auto">
-          <Text type="subTitle" color="var(--main)" marginBottom="15px">
-            {resultData[0].dog_name}에게 필요한 것
-          </Text>
-          <Grid flexWrap="wrap" width="100%" is_flex margin="0 0 3rem 0">
-            {resultData[0].needs.map((need, index) => {
-              return (
-                <Dropdown title={need.title} desc={need.desc} key={index} />
-              );
-            })}
-          </Grid>
+      </Grid>
+      <Grid margin="2rem auto">
+        <Text type="subTitle" color="var(--main)" marginBottom="15px">
+          {resultData[0].dog_name}의 솔루션
+        </Text>
+        <Text type="body" color="var(--deepcream)">
+          {resultData[0].solution[1]}
+          &nbsp;
+          {resultData[0].solution[2]}
+          &nbsp;
+          {resultData[0].solution[3]}
+          &nbsp;
+          {resultData[0].solution[4]}
+        </Text>
+      </Grid>
+      <Grid margin="2rem auto">
+        <Text type="subTitle" color="var(--main)" marginBottom="15px">
+          {resultData[0].dog_name}에게 필요한 것
+        </Text>
+        <Grid flexWrap="wrap" width="100%" is_flex margin="0 0 3rem 0">
+          {resultData[0].needs.map((need, index) => {
+            return <Dropdown title={need.title} desc={need.desc} key={index} />;
+          })}
         </Grid>
+      </Grid>
 
-        <Grid margin="2rem auto">
-          <Text type="subTitle" color="var(--main)" marginBottom="15px">
-            오늘의 감정일기(메모)
+      <Grid margin="2rem auto">
+        <Text type="subTitle" color="var(--main)" marginBottom="15px">
+          오늘의 감정일기(메모)
+        </Text>
+        <Input multiLine placeholder="반려인의 간단한 기록 작성 공간" />
+      </Grid>
+      <Grid is_flex margin="1rem auto">
+        <Button
+          width="48%"
+          padding="0.5rem"
+          bg="var(--main)"
+          color="var(--white)"
+          radius="5px"
+          cursor
+          _onClick={() => history.push('/main')}
+        >
+          <Text type="button" color="var(--white)">
+            검사 다시하기
           </Text>
-          <Input multiLine placeholder="반려인의 간단한 기록 작성 공간" />
-        </Grid>
-        <Grid is_flex margin="1rem auto">
-          <Button
-            width="48%"
-            padding="0.5rem"
-            bg="var(--main)"
-            color="var(--white)"
-            radius="5px"
-            cursor
-            _onClick={() => history.push('/main')}
-          >
-            <Text type="button" color="var(--white)">
-              검사 다시하기
-            </Text>
-          </Button>
-          <Button
-            width="48%"
-            padding="0.5rem"
-            bg="var(--main)"
-            color="var(--white)"
-            radius="5px"
-            cursor
-            _onClick={() => history.push('/mypet')}
-          >
-            <Text type="button" color="var(--white)">
-              마이펫 페이지
-            </Text>
-          </Button>
-        </Grid>
-      </Container>
-
-      {modalOpen && (
-        <Modal width="35rem" height="50rem" setOpenModal={setModalOpen}>
-          <AddPetModal />
-        </Modal>
-      )}
-    </>
+        </Button>
+        <Button
+          width="48%"
+          padding="0.5rem"
+          bg="var(--main)"
+          color="var(--white)"
+          radius="5px"
+          cursor
+          _onClick={() => history.push('/mypet')}
+        >
+          <Text type="button" color="var(--white)">
+            마이펫 페이지
+          </Text>
+        </Button>
+      </Grid>
+    </Container>
   );
 };
 
