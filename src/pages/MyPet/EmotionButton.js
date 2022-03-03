890ef814d16945d7f9e9e86187fb21faa2c8a983
leaisrevolution <leaisrevolution@gmail.com>
2022-03-03 17:14:41 +0900

    [#14] feat : emotion button 추가

---
+++ src/pages/MyPet/EmotionButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+//compo
+import { Button } from '../../common/components';
+
+export const EmotionButton = () => {
+  return <div>EmotionButton</div>;
+};
