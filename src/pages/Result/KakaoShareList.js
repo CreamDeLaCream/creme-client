dbb43ef967a2e75e288d497ca3c4010df2253909
greencoffee1635 <166354@naver.com>
2022-03-10 20:52:55 +0900

    [#63] edit: 오타수정

---
+++ src/pages/Result/KakaoShareList.js
@@ -0,0 +1,40 @@
+const appKey = process.env.REACT_APP_KAKAO_SHARE_KEY;
+
+const KakaoShareList = (
+  title = '오늘의 마이펫',
+  description = '나의 반려견의 표정은?',
+  imgUrl = process.env.PUBLIC_URL + `/Image/partner1.png`,
+  link = window.location.href,
+  buttonTitle = '보러 가기',
+) => {
+  if (window.Kakao) {
+    const kakao = window.Kakao;
+    if (!window.Kakao.isInitialized()) {
+      kakao.init(appKey);
+    }
+    kakao.Link.sendDefault({
+      // container: '#test',
+      objectType: 'feed',
+      content: {
+        title: title,
+        description: description,
+        imageUrl: imgUrl,
+        link: {
+          webUrl: link,
+          mobileWebUrl: link,
+        },
+      },
+      buttons: [
+        {
+          title: buttonTitle,
+          link: {
+            webUrl: link,
+            mobileWebUrl: link,
+          },
+        },
+      ],
+    });
+  }
+};
+
+export default KakaoShareList;
