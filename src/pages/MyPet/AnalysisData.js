afe9d0b1641954489f3856ea42ebd8ad9cd47a5c
leaisrevolution <leaisrevolution@gmail.com>
2022-03-11 11:43:31 +0900

    [#14] edit : 더미데이터

--- src/pages/MyPet/AnalysisData.js
+++ src/pages/MyPet/AnalysisData.js
@@ -62,6 +62,41 @@ export const AnalysisData = [
     isLiked: true,
     emotion: 'angry',
   },
+  {
+    id: 9,
+    name: 'summer',
+    date: '2022/02/22',
+    isLiked: true,
+    emotion: 'angry',
+  },
+  {
+    id: 10,
+    name: 'lulu',
+    date: '2022/02/22',
+    isLiked: true,
+    emotion: 'happy',
+  },
+  {
+    id: 11,
+    name: 'summer',
+    date: '2022/02/22',
+    isLiked: true,
+    emotion: 'happy',
+  },
+  {
+    id: 12,
+    name: 'summer',
+    date: '2022/02/22',
+    isLiked: true,
+    emotion: 'angry',
+  },
+  {
+    id: 13,
+    name: 'summer',
+    date: '2022/02/22',
+    isLiked: true,
+    emotion: 'happy',
+  },
 ];
 
 export default AnalysisData;
